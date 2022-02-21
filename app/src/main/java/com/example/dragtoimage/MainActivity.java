4078033acdde19633a3234864e5ced07bdd679a6
eytanBukris123 <eytan.bukris@gmail.com>
2022-02-21 14:41:58 +0200

    l

--- app/src/main/java/com/example/dragtoimage/MainActivity.java
+++ app/src/main/java/com/example/dragtoimage/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.dragtoimage;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -14,7 +16,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
-    ImageView poop, dote;
+    ImageView Toilet, Poop;
     float xDown = 0;
     float yDown = 0;
 
@@ -28,39 +30,44 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     int fallSpeed = 5000;
 
+    boolean stopThread;
+
     float bottomOfScreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        poop = findViewById(R.id.poop);
-        dote = findViewById(R.id.dote);
+        Toilet = findViewById(R.id.poop);
+        Poop = findViewById(R.id.dote);
         pointsTv = findViewById(R.id.pointsTv);
         pointsTv.setAlpha((float) 0.5);
 
-        poop.setOnTouchListener(this);
+        Toilet.setOnTouchListener(this);
 
-        yAbs = (int)dote.getY();
+        yAbs = (int) Poop.getY();
         displaymetrics = new DisplayMetrics();
 
         bottomOfScreen = getResources().getDisplayMetrics()
-                .heightPixels - (dote.getHeight() * 4);
+                .heightPixels - (Poop.getHeight() * 4);
 
-        dote.animate()
+        Poop.animate()
                 .translationY(bottomOfScreen)
                 .setInterpolator(new AccelerateInterpolator())
                 .setDuration(fallSpeed);
 
-    }
+        startThread();
 
+
+    }
 
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
 
-
+        touchAndRestart();
 
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
@@ -75,38 +82,94 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 float distanceX = movedX -xDown;
                 float distanceY = movedY -yDown;
 
-                poop.setX(poop.getX() + distanceX);
-                //poop.setY(poop.getY() + distanceY);
-
+                Toilet.setX(Toilet.getX() + distanceX);
 
                 break;
 
             case MotionEvent.ACTION_UP:
         }
 
-        rc1 = new Rect();
-        poop.getHitRect(rc1);
-        rc2 = new Rect();
-        dote.getHitRect(rc2);
 
-        if (Rect.intersects(rc1, rc2)){
-            int random = (int)(Math.random()*1000);
-            fallSpeed -= 500;
-            if(fallSpeed <= 0){
-                fallSpeed = 500;
-            }
-            dote.setX(random);
-            dote.setY(yAbs);
-            dote.animate()
-                    .translationY(bottomOfScreen)
-                    .setInterpolator(new AccelerateInterpolator())
-                    .setDuration(fallSpeed);
-            Points++;
-            pointsTv.setText("Points: " + Points);
-
-        }
 
         return true;
     }
+
+
+    public void startThread() {
+        stopThread = false;
+        TouchThread runnable = new TouchThread(this);
+        new Thread(runnable).start();
+
+    }
+
+    public void stopThread() {
+        stopThread = true;
+        restart();
+    }
+
+    public void restart(){
+        int random = (int)(Math.random()*1000);
+        fallSpeed -= 250;
+        if(fallSpeed <= 250){
+            fallSpeed = 500;
+        }
+        Poop.setX(random);
+        Poop.setY(yAbs);
+        Poop.animate()
+                .translationY(bottomOfScreen)
+                .setInterpolator(new AccelerateInterpolator())
+                .setDuration(fallSpeed);
+        Points++;
+        pointsTv.setText("Points: " + Points);
+    }
+
+    public boolean touch(){
+        rc1 = new Rect();
+        Toilet.getHitRect(rc1);
+        rc2 = new Rect();
+        Poop.getHitRect(rc2);
+
+        if (Rect.intersects(rc1, rc2)){
+
+            return true;
+
+        }
+        return false;
+    }
+
+    public void touchAndRestart(){
+        rc1 = new Rect();
+        Toilet.getHitRect(rc1);
+        rc2 = new Rect();
+        Poop.getHitRect(rc2);
+
+        if (touch()){
+
+            restart();
+
+        }
+    }
 }
+
+    class TouchThread extends Thread {
+
+    boolean always = false;
+
+    Context context;
+
+    TouchThread(Context context){
+        this.context = context;
+    }
+
+        @Override
+        public void run() {
+
+            while(!always) {
+                if(((MainActivity)context).touch()){
+                    ((MainActivity)context).stopThread();
+//                    ((MainActivity)context).restart();
+                }
+            }
+        }
+    }
 
